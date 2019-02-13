eee2bd7a803f86d129fbb062ee2a200cbbc3d4bd
dmitrysarov <d.shaulsky@millionagents.com>
2019-02-13 15:28:35 +0300

    add rotating

--- logging_example.py
+++ logging_example.py
@@ -3,12 +3,13 @@ from logging import handlers
 
 logger = logging.getLogger(__name__)
 handler_stream = logging.StreamHandler() #output to console
-#logFile = 'example.log'
-#handler_file = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
-#                                        backupCount=1, encoding=None, delay=0)
+logFile = 'ocr.log'
+handler_file = handlers.RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
+                                        backupCount=1, encoding=None, delay=0)
 formatter = logging.Formatter("%(asctime)s:::%(module)s:::%(message)s")
 handler_stream.setFormatter(formatter)
+handler_file.setFormatter(formatter)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler_stream)
+logger.addHandler(handler_file)
 logger.info('hello world')
-
